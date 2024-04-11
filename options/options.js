025e629332f28d7637a39e72f6fd94742b70a5e9
Crowdbotics <team@crowdbotics.com>
2024-04-11 17:21:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shareandsolve-test-47471",
+  url: "https://shareandsolve-test-47471.botics.co",
+  api: "https://shareandsolve-test-47471.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
